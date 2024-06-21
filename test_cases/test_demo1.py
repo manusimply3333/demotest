ebf15b3e3802720536b3c091f6d30b94580fbe86
Manoj Kumar <mkumar@perforce.com>
2024-06-21 17:26:10 +0530

    initial commit

---
+++ test_cases/test_demo1.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+def test1():
+    print("This is Test1, it will pass")
+
+def test2():
+    print("This is Test2, This will fail")
+    assert 1 == 2,"wrong assertion"
+
+def test3():
+    print("This is Test3, it will run for loop")
+    for i in range(0,10):
+        print(f"The value is {i}")
